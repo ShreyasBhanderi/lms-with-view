1f4a1194976b15f7ac7a159ef19f074a66bc8188
Shreyas Bhanderi <shreyas.bhanderi@nagarro.com>
2018-05-15 01:54:08 +0530

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path_1 = __importDefault(require("path"));
 const routes_1 = __importDefault(require("./routes/routes"));
 const app = express_1.default();
 
-const port  = process.env.PORT;
+const port  = process.env.PORT || 5000;
 app.get('/',function(req,res){
     res.send("Welcome");
 });
