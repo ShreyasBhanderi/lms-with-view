54d11b608e17284310801c8bd5fec18f98a9318a
Shreyas Bhanderi <shreyas.bhanderi@nagarro.com>
2018-05-15 01:10:39 +0530

    make it better

--- routes/subjects.js
+++ routes/subjects.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const subject_1 = __importDefault(require("../models/subject"));
+const subject_1 = __importDefault(require("../models/Subject"));
 const teacher_1 = __importDefault(require("../models/teacher"));
 class SubjectRouter {
     constructor() {
