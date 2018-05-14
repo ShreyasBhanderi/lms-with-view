ff4026910e902d95d1689c8626ecd8219e75d8fa
Shreyas Bhanderi <shreyas.bhanderi@nagarro.com>
2018-05-15 02:01:23 +0530

    make it better

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const routes_1 = __importDefault(require("./routes/routes"));
 const app = express_1.default();
 
 const port  = process.env.PORT || 5000;
-app.get('/',function(req,res){
-    res.send("Welcome");
-});
+
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use('/', express_1.default.static(path_1.default.join(__dirname, 'public')));
