62f51ae99934e7ca8c4f76400b586b5c0af12c89
Shreyas Bhanderi <shreyas.bhanderi@nagarro.com>
2018-05-15 01:02:43 +0530

    make it better

--- models/db.js
+++ models/db.js
@@ -4,9 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = __importDefault(require("sequelize"));
-const db = new sequelize_1.default('lms', 'jarvis', 'ultron', {
-    host: 'localhost',
-    dialect: 'mysql',
+const db = new sequelize_1.default('d1u8044050it0', 'jgarhrcfoklwkd', '6824b008531d48a91dc245fde414d6f38ae0eee958c3208e30e37ce8a4ecd624', {
+    host: 'ec2-50-19-224-165.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    port: '5432',
     pool: {
         min: 0,
         max: 5,
